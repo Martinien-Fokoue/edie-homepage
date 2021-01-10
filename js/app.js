c3d4ec0925419d24ca0079d04ff7ea395f0e4f1a
Martinien Fokoue <martinienfokoue@gmail.com>
2021-01-10 05:26:33 +0100

    modifications

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 let hamburgerBtn = document.querySelector('.hamburger_btn'),
-    nav = document.querySelector('nav');
+    nav = document.querySelector('nav'),
+    nav_links = document.querySelectorAll('nav a');
 
     hamburgerBtn.addEventListener('click', function(){
         this.classList.toggle('active');
         nav.classList.toggle('active');
     });
+
+nav_links.forEach(link => {
+    link.addEventListener('click', function(){
+        if(nav.classList.contains('active')){
+            nav.classList.remove('active');
+            hamburgerBtn.classList.remove('active');
+        }
+    });
+});
